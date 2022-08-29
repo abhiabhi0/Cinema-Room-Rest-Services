aa2b2a5eeac2c0727101a4da9a3797084fc99388
programmercave0 <mailabhi593@gmail.com>
2022-08-29 14:58:30 +0530

    purchase method added in controller

--- src/main/java/com/programmercave/cinema/Controller/CinemaController.java
+++ src/main/java/com/programmercave/cinema/Controller/CinemaController.java
@@ -5,6 +5,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.programmercave.cinema.Model.Room;
+import com.programmercave.cinema.Model.Seat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 import static com.programmercave.cinema.Model.Room.getAllSeats;
 
@@ -19,5 +24,25 @@ public class CinemaController {
     @GetMapping("/seats")
     public Room getSeats() {
         return room;
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<?> purchase(@RequestBody Seat seat) {
+        if (seat.getColumn() > room.getTotalColumns()
+            || seat.getRow() > room.getTotalRows()
+            || seat.getRow() < 1
+            || seat.getColumn() < 1) {
+
+            return new ResponseEntity<>(Map.of("error", "The number of a row or a column is out of bounds!"), HttpStatus.BAD_REQUEST);
+        }
+
+        for (int i = 0; i < room.getAvailableSeats().size(); ++i) {
+            Seat s = room.getAvailableSeats().get(i);
+            if (s.equals(seat)) {
+                room.getAvailableSeats().remove(i);
+                return new ResponseEntity<>(s, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(Map.of("error", "The ticket has been already purchased!"), HttpStatus.BAD_REQUEST);
     }
 }
